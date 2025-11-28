189e6dca3aa8ef724e35104299df53a89be3b814
Micheal McErlean <MichealMcErlean@users.noreply.github.com>
2025-11-28 20:07:12 +1100

    Initial commit
    Cleaned out the example code, set up first component file, got something showing on the dev server.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { GeneralSection } from './components/Section'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="form">
+      <h1>CV Application</h1>
+      <h2>An Odin Project Exercise</h2>
+      <GeneralSection />
+    </div>
+  );
+}
+export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+import '../styles/Section.css'
+
+export function GeneralSection() {
+
+
+  return (
+    <div className="section">
+      <h2>General Information</h2>
+    </div>
+  );
+}
